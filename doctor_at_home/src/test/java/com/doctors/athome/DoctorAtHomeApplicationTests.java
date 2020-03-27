198a3a0b3964d5dbead598b27fedd5c518a0bd72
sha33415 <abidemi.shobayoeni@gmail.com>
2020-03-27 20:22:47 +0100

    Spring Boot project created with mongodb and twilio dependencies

---
+++ doctor_at_home/src/test/java/com/doctors/athome/DoctorAtHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctors.athome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAtHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
